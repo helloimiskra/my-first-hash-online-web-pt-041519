4b891ef7a2ecdfa0a6ecd8978e42d530388278da
Iskra B <ibatistapoblete@gmail.com>
2019-04-19 03:32:40 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -34,18 +34,19 @@ def adding
                   "porcelain vases" => 2,
                   "oil paintings" => 3
                  }
+                 shipping_manifest["muskets"] = 2
+                 shipping_manifest["gun powder"] = 4
+               end
+               puts shipping_manifest
+             end
   # add 2 muskets to the shipping_manifest hash below
 
-    shipping_manifest["muskets"] = 2
+                
 
 
 
   # add 4 gun powder to the shipping_manifest hash below
-    shipping_manifest["gun powder"] = 4
-  end
+  
 
 
   # return the shipping_manifest hash below
-puts shipping_manifest
-
-end
